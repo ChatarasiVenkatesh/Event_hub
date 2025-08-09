e2d86fa6ae6d7283c4004a60e9bea77e3d8d220b
Venkatesh <chvenkateswararaovenky@gmail.com>
2025-08-09 17:37:35 +0530

    codegnan text footer2

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -114,7 +114,7 @@ export const Footer = () => {
             <span>© {currentYear} EventHub. Made with</span>
             <Heart className="h-4 w-4 text-red-500 fill-current" />
             <span>
-              for <b className="text-black text-xl">Codegnan lovers</b>.
+              for <b className="text-black text-xl">Codegnan lovers ...</b>
             </span>
           </div>
 
